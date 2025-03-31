450a051f27b7f743c3075f1f0571160682291be5
dikshasingh1234 <diksha.singh@wezenite.com>
2025-03-31 21:40:00 +0530

    Recent 31 march updates

--- js/main.js
+++ js/main.js
@@ -276,11 +276,7 @@
     fixedContentPos: false
   });
 
-  $('.appointment_date').datepicker({
-	  'format': 'm/d/yyyy',
-	  'autoclose': true
-	});
-	$('.appointment_time').timepicker();
+  
 
 
 
